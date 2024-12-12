a2c2f7647174b5bdfd3b3d36744562b769506578
Jayden Kim <hkim@lcs.on.ca>
2024-12-12 11:22:33 -0500

    Initial Commit

---
+++ MyFavoriteThings/MyFavoriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavoriteThingsApp.swift
+//  MyFavoriteThings
+//
+//  Created by kim heejun on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFavoriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
